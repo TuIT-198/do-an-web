362100d454eb0f7fd18572ec7a8d4762ca95d4c7
TuIT <congtuu198@gmail.com>
2026-03-27 17:46:29 +0700

    build UI for header

--- src/components/HeaderComponent/HeaderComponent.jsx
+++ src/components/HeaderComponent/HeaderComponent.jsx
@@ -1,7 +1,52 @@
 import React from "react";
-
+import { Col, Flex } from "antd";
+import { WrapperTextHeader } from "./style";
+import { WrapperHeader } from "./style";
+import Search from "antd/es/transfer/search";
+import { UserOutlined } from "@ant-design/icons";
+import { CaretDownOutlined } from "@ant-design/icons";
+import { WrapperHeaderAccount } from "./style";
+import { ShoppingCartOutlined } from "@ant-design/icons";
+import { WrapperTextHeaderSmall } from "./style";
+import { Color } from "antd/es/color-picker";
 const HeaderComponent = () => {
-  return <div>HeaderComponent</div>;
+  return (
+    <div>
+      <WrapperHeader gutter={"15px"}>
+        <Col span={6}>
+          {" "}
+          <WrapperTextHeader> TuIT </WrapperTextHeader>
+        </Col>
+        <Col span={12}>
+          <Search
+            placeholder="input search text"
+            allowClear
+            enterButton="Search"
+            size="large"
+            // onSearch={onSearch}
+          />
+        </Col>
+        <Col span={6} style={{ display: "flex", gap: "20px" }}>
+          <WrapperHeaderAccount>
+            <UserOutlined style={{ fontSize: "30px" }} />
+            <div>
+              <WrapperTextHeaderSmall>
+                Đăng nhập/ Đăng ký
+              </WrapperTextHeaderSmall>
+              <div>
+                <WrapperTextHeaderSmall>Tài khoản </WrapperTextHeaderSmall>
+                <CaretDownOutlined />
+              </div>
+            </div>
+          </WrapperHeaderAccount>
+          <div>
+            <ShoppingCartOutlined style={{ fontSize: "30px", color: `#fff` }} />
+            <WrapperTextHeaderSmall>Giỏ hàng</WrapperTextHeaderSmall>
+          </div>
+        </Col>
+      </WrapperHeader>
+    </div>
+  );
 };
 
 export default HeaderComponent;
---
+++ src/components/HeaderComponent/style.js
@@ -0,0 +1,24 @@
+import { Row } from "antd";
+import styled from "styled-components";
+
+export const WrapperHeader = styled(Row)`
+  padding: 10px 120px;
+  background-color: #2810fb;
+  align-items: center;
+`;
+export const WrapperTextHeader = styled.span`
+  font-size: 20px;
+  color: #fff;
+  font-weight: bold;
+  text-align: left;
+`;
+export const WrapperHeaderAccount = styled.div`
+  display: flex;
+  align-items: center;
+  color: #fff;
+  gap: 10px;
+`;
+export const WrapperTextHeaderSmall = styled.span`
+  font-size: 12px;
+  color: #fff;
+`;
